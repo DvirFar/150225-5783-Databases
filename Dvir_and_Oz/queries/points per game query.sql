129a33285ed6e63c253eac5fec0279dc8e7ff8f3
DvirFar <112068209+DvirFar@users.noreply.github.com>
2023-06-12 15:20:10 +0300

    Update points per game query.sql

--- Dvir_and_Oz/queries/points per game query.sql
+++ Dvir_and_Oz/queries/points per game query.sql
@@ -1,6 +1,8 @@
-select p.id, p.fname || ' ' || p.lname as player_name, sum(t.score)/count(t.game_id) as points_per_game
-from liocohen.player p
-join oreich.throw t on t.thrower_id = p.id
-group by p.id, p.fname, p.lname
-order by points_per_game desc
-fetch first 50 rows only
+SELECT p.id, p.fname || ' ' || p.lname AS player_name, t.team_name, SUM(th.score)/COUNT(th.game_id) AS points_per_game
+FROM liocohen.player p
+JOIN oreich.throw th ON th.thrower_id = p.id
+JOIN oreich.play_in pla ON pla.player_id = th.thrower_id
+JOIN chashken.team t ON t.teamid = pla.team_id
+GROUP BY p.id, p.fname, p.lname, t.team_name
+HAVING SUM(th.score)/COUNT(th.game_id) > 1.55
+ORDER BY points_per_game DESC, p.fname || ' ' || p.lname ASC
